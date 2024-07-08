b86bfdd2deaaf6f49b951a56cdff90a741b33258
Himanshu verma <98012299+TechisHeaven@users.noreply.github.com>
2024-07-09 02:14:47 +0530

    Cleaning or seperating Logic From main function With Structs and arguments passing with variables and references

--- command_line/minigrep/src/main.rs
+++ command_line/minigrep/src/main.rs
@@ -3,12 +3,27 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let value = &args[1];
-    let file_path = &args[2];
-    println!("Arguments: {:?}", value);
-    println!("File Path :{:?}", file_path);
+    let config = parse_arguments(&args);
+    println!("Arguments: {:?}", config.value);
+    println!("File Path :{:?}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("should have been able to read the file");
 
     println!("with text:\n {:?}", contents);
+}
+
+struct Config {
+    value: String,
+    file_path: String,
+}
+
+fn parse_arguments(args: &[String]) -> Config {
+    let value: &String = &args[1];
+    let file_path: &String = &args[2];
+
+    Config {
+        value: value.to_string(),
+        file_path: file_path.to_string(),
+    }
 }
